320d076d5393cf88f9c208554fcd60153445190f
SriGiriT <srigirit369@gmail.com>
2023-04-11 21:20:02 +0530

    sos button

--- lib/screens/bluetooth.dart
+++ lib/screens/bluetooth.dart
@@ -165,7 +165,10 @@ class _BluetoothScreenState extends State<BluetoothScreen> {
                       sendSms(
                           "Emergency Alert\nHeart Rate-$heartRate\nBp-$bp\nAir Humidity-$air\nTemperature-$temp\nSPO2-$sp02");
                     },
-                    icon: Icon(Icons.textsms)),
+                    icon: Icon(
+                      Icons.sos,
+                      color: Colors.red,
+                    )),
                 IconButton(
                   icon: Icon(Icons.info),
                   onPressed: () {
@@ -444,189 +447,22 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
             ),
             SizedBox(height: 16.0),
             Expanded(
-                child: devices.length > 0
-                    ? ListView.builder(
-                        itemCount: devices.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return ListTile(
-                            title: Text(devices[index].name ?? "unknown"),
-                            subtitle: Text(devices[index].address),
-                            onTap: () {
-                              Navigator.of(context).pop(devices[index]);
-                            },
-                          );
-                        },
-                      )
-                    : Text("no")),
+              child: ListView.builder(
+                itemCount: devices.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(devices[index].name ?? "unknown"),
+                    subtitle: Text(devices[index].address),
+                    onTap: () {
+                      Navigator.of(context).pop(devices[index]);
+                    },
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-
-class HC05Bluetooth {
-  late BluetoothDevice _device;
-  late BluetoothCharacteristic _characteristic;
-
-  Future<List<BluetoothDevice>> scanForDevices() async {
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    List<BluetoothDevice> devices = [];
-
-    flutterBlue.scan(timeout: Duration(seconds: 4)).listen((scanResult) {
-      BluetoothDevice device = scanResult.device;
-      if (!devices.contains(device)) {
-        devices.add(device);
-      }
-    });
-
-    await Future.delayed(Duration(seconds: 4));
-    return devices;
-  }
-
-  Future<bool> connect(String deviceId, String password) async {
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    List<BluetoothDevice> devices = await scanForDevices();
-    // List<BluetoothDevice> devices = await flutterBlue.scan(timeout: Duration(seconds: 4));
-    for (BluetoothDevice device in devices) {
-      if (device.id.toString() == deviceId) {
-        _device = device;
-        break;
-      }
-    }
-
-    if (_device == null) {
-      print("Device not found");
-      return false;
-    }
-
-    await _device.connect();
-
-    List<BluetoothService> services = await _device.discoverServices();
-    for (BluetoothService service in services) {
-      if (service.uuid.toString() == "00001101-0000-1000-8000-00805F9B34FB") {
-        List<BluetoothCharacteristic> characteristics = service.characteristics;
-        for (BluetoothCharacteristic characteristic in characteristics) {
-          if (characteristic.uuid.toString() ==
-              "00001101-0000-1000-8000-00805F9B34FB") {
-            _characteristic = characteristic;
-            break;
-          }
-        }
-        break;
-      }
-    }
-
-    if (_characteristic == null) {
-      print("Could not find HC05 characteristic");
-      return false;
-    }
-
-    await _characteristic.write(utf8.encode(password));
-    await _characteristic.setNotifyValue(true);
-
-    return true;
-  }
-
-  Future<void> sendData(String data) async {
-    await _characteristic.write(utf8.encode(data));
-  }
-}
-
-
-class BluetoothScreen extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final TextEditingController _textController = TextEditingController();
-  late HC05Bluetooth _bluetooth;
-  bool _isConnected = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _bluetooth = HC05Bluetooth();
-  }
-
-  Future<void> _connect() async {
-    bool isConnected = await _bluetooth.connect("HC05 Device ID", "Password");
-    setState(() {
-      _isConnected = isConnected;
-    });
-  }
-
-  Future<void> _sendData() async {
-    String data = _textController.text;
-    await _bluetooth.sendData(data);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bluetooth Demo'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _textController,
-                decoration: InputDecoration(
-                  hintText: 'Enter data to send',
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _isConnected ? _sendData : null,
-                child: Text('Send'),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _connect,
-          tooltip: 'Connect',
-          child: Icon(Icons.bluetooth),
-        ),
-      ),
-    );
-  }
-}
-*/
